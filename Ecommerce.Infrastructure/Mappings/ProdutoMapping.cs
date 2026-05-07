e9eae0cf2ed47f031e477a6eb56360a90038305d
Glauber <glauber.charles2012@gmail.com>
2026-05-07 16:52:09 -0300

    Primeiro commit do projeto de ecommerce.

---
+++ Ecommerce.Infrastructure/Mappings/ProdutoMapping.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produtos");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Descricao)
+                .HasMaxLength(500);
+
+            builder.Property(x => x.Preco)
+                .HasColumnType("decimal(18,2)");
+            
+            builder.Property(x => x.Estoque)
+                .IsRequired();
+
+            builder.Property(x => x.DataCadastro)
+                .IsRequired();
+
+        }
+    }
+}
