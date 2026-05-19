f2b85c1cb1a343e7dd9f48afc84822652edd3207
Glauber <glauber.charles2012@gmail.com>
2026-05-19 16:47:47 -0300

    Feita a parte de criação de produtos

---
+++ Ecommerce/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Web.ViewModels
+{
+    public class ProdutoViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Informe a descrição")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço")]
+        public decimal Preco { get; set; }
+
+        [Required(ErrorMessage = "Informe o estoque")]
+        public int Estoque { get; set; }
+
+    }
+}
